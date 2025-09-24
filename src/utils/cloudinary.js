39979b5cb694d6d6ee9b9a4e03b81df3a02bd019
engrab <engrbilawalhussain@gmail.com>
2025-09-24 10:31:05 +0500

    middleware and cloudinary integration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,70 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+import fs from "fs"
+
+ // Configuration
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+// this is IIFE
+// (async function () {
+
+//     // Configuration
+//     cloudinary.config({
+//         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//         api_key: process.env.CLOUDINARY_API_KEY,
+//         api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+//     });
+
+//     // Upload an image
+//     const uploadResult = await cloudinary.uploader
+//         .upload(
+//             'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//             public_id: 'shoes',
+//         }
+//         )
+//         .catch((error) => {
+//             console.log(error);
+//         });
+
+//     console.log(uploadResult);
+
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+
+//     console.log(optimizeUrl);
+
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+
+//     console.log(autoCropUrl);
+// })();
+
+const uploadOnCloudinary = async (localPath) => {
+
+    try {
+
+        if (!localPath) return nulll;
+        // upload the file on cloudinary
+        const response = await cloudinary.v2.uploader.upload(localPath, { resource_type: "auto" })
+        console.log("file uploaded succesfully", response.url)
+        return response
+
+    } catch (error) {
+
+        fs.unlinkSync(localPath) // remove corrupted file from our server
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
