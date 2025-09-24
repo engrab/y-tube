9675d8052c8ae856f302e4c4b82c647da5951b57
engrab <engrbilawalhussain@gmail.com>
2025-09-24 23:02:06 +0500

    config integration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,11 +6,11 @@ import path from "path"
 
 
 // Configuration
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
-// });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
 // this is IIFE
 // (async function () {
 
